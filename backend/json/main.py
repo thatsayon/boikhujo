fbcc46ab5909f1b0a1811f0f2ec1e269da0c2ef0
thatsayon <ashiqulislamayon28@gmail.com>
2023-07-05 23:29:54 +0600

    First version of api is ready

--- backend/json/main.py
+++ backend/json/main.py
@@ -1,4 +1,8 @@
 from flask import Flask, jsonify, request
+import json
+import sys 
+sys.path.insert(1, '../scraping')
+from scraping import save_json
 
 app = Flask(__name__)
 
@@ -11,7 +15,15 @@ def home():
 @app.route('/book', methods = ['GET'])
 def books():
     book_name = request.args.get('name')
-    return jsonify({'name':book_name})
 
+    return save_json(book_name)
+    
+
+@app.route('/res', methods=['GET'])
+def res():
+    f = open('../scraping/data.json')
+
+    data = json.load(f)
+    return jsonify(data)
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/scraping/scraping.py
+++ backend/scraping/scraping.py
@@ -1,1 +1,40 @@
-print("HI")
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import pandas as pd
+import json 
+
+book_list = []
+def data_from_daraz(name):
+    bs = len(name)
+    url = f'https://www.daraz.com.bd/catalog/?q={name}'
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+
+    elements = driver.find_elements(By.CLASS_NAME, 'info--ifj7U')
+    for element in elements:
+        book_name_a = element.find_element(By.TAG_NAME, 'a')
+        book_name = book_name_a.text
+        link = book_name_a.get_attribute('href')
+
+        price = element.find_element(By.CSS_SELECTOR, 'span.currency--GVKjl')
+        price_text = price.text
+
+        new_book_name = book_name[:bs]
+        if new_book_name.lower() == name.lower():
+            result = {
+                'book_name': book_name,
+                'price': price_text[2:],
+                'href': link
+            }
+
+            book_list.append(result)
+    driver.quit()
+
+def save_json(book_name):
+    data_from_daraz(book_name)
+    sorted_book_list = sorted(
+        book_list, key=lambda x: int(x['price'].replace(',', '')))
+
+    final_res = json.dumps(sorted_book_list)
+    return final_res
