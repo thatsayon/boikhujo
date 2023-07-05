abeea76538eff86babf7fe1319143e0e0a9ee4ab
thatsayon <ashiqulislamayon28@gmail.com>
2023-07-05 16:13:40 +0600

    First Step Done

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { Search } from 'chakra-ui-search'
+import { FaSearch } from "react-icons/fa";
+import React, { useState } from 'react'
 import './HomeStyles.css'
 function Home() {
+  const [book, setBook] = useState("");
   return (
     <div>
-      <div className='bg'></div>
+      <div className='bg'>
+        <div className="input-wrapper">
+          <input type="text" placeholder='Book name..' onChange={(e) => setBook(e.target.value)}/>
+
+          <FaSearch id='search-icon' onClick={() => { console.log(book) }} />
+        </div>
+      </div>
     </div>
   )
 }
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,29 @@
 import { Outlet, Link } from "react-router-dom";
+import './NavbarStyles.css'
 const Navbar = () => {
     return (
         <div>
             <nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/blogs">Blogs</Link>
-                    </li>
-                    <li>
-                        <Link to="/contact">Contact</Link>
-                    </li>
-                </ul>
+                <div className="main">
+                    <div>
+                        <a>Boi Khujo</a>
+                    </div>
+                    <div>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/blogs">Blogs</Link>
+                            </li>
+                            <li>
+                                <Link to="/contact">Contact</Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="user">
+                    </div>
+                </div>
             </nav>
 
             <Outlet />
