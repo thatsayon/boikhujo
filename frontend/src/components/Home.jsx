6d1b7d4862f822d214cc232f24c4f3d5948c4bab
thatsayon <ashiqulislamayon28@gmail.com>
2023-07-06 13:15:21 +0600

    First Step Complete Properly

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,26 +1,37 @@
 import { Search } from 'chakra-ui-search'
 import { FaSearch } from "react-icons/fa";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, Button } from 'react'
 import axios from 'axios';
 import './HomeStyles.css'
 import DataTable from './DataTable';
-
+import BasicUsage from './LoadingModal';
 function Home() {
   const [book, setBook] = useState("");
   const [bookdata, setBookdata] = useState([])
-
+  const [loading, setLoading] = useState(false)
   const fetchInfo = () => {
     return axios.get(`http://127.0.0.1:5000/book?name=${book}`)
-      .then((res) => setBookdata(res.data));
+      .then((res) => {setBookdata(res.data); setLoading(false)})
   }
 
+  const handleKeyDown = (event) => {
+    if (event.key == 'Enter') {
+      fetchInfo();
+    }
+  }
   return (
     <div>
+      {loading && <BasicUsage />}
       <div className='bg'>
         <div className="input-wrapper">
-          <input type="text" placeholder='Book name..' onChange={(e) => setBook(e.target.value)} />
+          <input
+            type="text"
+            placeholder='Book Name...'
+            onChange={(e) => setBook(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
 
-          <FaSearch id='search-icon' onClick={() => { fetchInfo() }} />
+          <FaSearch id='search-icon' onClick={() => { fetchInfo(); setLoading(true) }} />
         </div>
       </div>
       <div className="table">
---
+++ frontend/src/components/LoadingModal.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./LoadingModal.css"
+const BasicUsage = () => {
+    return  (
+        <div className="popup">
+            <div className="popup-inner">
+                <h1>Your Data is Loading....</h1>
+            </div>
+        </div>
+    );
+}
+
+export default BasicUsage;
